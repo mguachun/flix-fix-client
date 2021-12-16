db97ae90eadabbe29f99a828fe5abd0246de93bd
Melissa Guachun <1212malka@gmail.com>
2021-12-16 14:22:17 -0500

    import howto and movieform

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import NavBar from "./components/NavBar";
 import './App.css';
 import About from "./containers/About";
 import Home from "./containers/Home";
+import HowTo from "./components/HowTo"
+import MovieForm from "./components/forms/MovieForm"
 function App() {
 
   return (
@@ -11,7 +13,10 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="movie-form" element={<MovieForm />} />
         <Route path="/about" element={<About />} />
+        <Route path="/how-to" element={<HowTo />} />
+        
     </Routes>
     </div>
   );
