5bbdf519f8671167d671906a8e419f2f50de85fd
Melissa Guachun <1212malka@gmail.com>
2021-12-16 14:23:09 -0500

    about summary

--- src/containers/About.js
+++ src/containers/About.js
@@ -3,7 +3,19 @@ import React from "react";
 
 const About = () => {
     return (
-      <div>About This Project</div>
+      <div>
+        <h1>About:</h1>
+        <p>This app was inspired by my boyfriend and his passion for films. On every
+          Sunday, he meets up with his friends online and they all watch a movie. Every 
+          person has a chance to pick a movie for the group and a common problem they face
+          is keeping track of the movies they've seen. Flix-Fix is an app to answer to this 
+          problem! 
+          
+         
+
+          
+        </p>
+        </div>
 
 
 
