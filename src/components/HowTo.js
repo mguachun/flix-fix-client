422412928ffc77b389e719398470a16bae63b794
Melissa Guachun <1212malka@gmail.com>
2021-12-16 14:22:27 -0500

    built out howto form

---
+++ src/components/HowTo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const HowTo = () => {
+    return (
+      <div>
+        <h1>How to use Flix-Fix:</h1>
+        <p>To log a movie, go to track a movie and complete the form.
+        Once the form is completed, hit send and navigate to the Home page. 
+        Home will contain all of your logged movies in chrronological order from 
+        oldest to newest. 
+
+        </p>
+        </div>
+
+
+
+    );
+  };
+export default HowTo;
