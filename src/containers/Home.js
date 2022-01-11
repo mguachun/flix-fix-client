eed9d3369acd1a045e7a9de848778d6ce03b71e8
Melissa Guachun <1212malka@gmail.com>
2022-01-11 12:30:47 -0500

    got rid of initial header

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,20 +10,12 @@ const Home = () => {
           
           textAlign: "center",
           fontSize: 21,
-          padding: "30px",
-          paddingRight: "20px",
-          paddingLeft: "20px",
-          borderStyle: "dashed",
-          borderRadius: "25px",
-          borderWidth: "2px",
-          borderColor: "#3621ad",
-          marginLeft: "20px",
-          marginRight: "20px",
+       
   
         }}
       >
-        <h1 style={{textAlign: "center"}}><strong>FlixFix</strong></h1> 
-        <h3 style={{textAlign: "center"}}>"<em>Movies for all"</em></h3>is an application that helps you keep track of 
+
+        <h3 style={{textAlign: "center"}}></h3>is an application that helps you keep track of 
         your movie queue. It allows you to view movies you've seen in the past and logs shows you have
         planned for the future. 
 
