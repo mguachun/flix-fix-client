449f051f6b23d77980e4d186a32f905f6428bf56
Melissa Guachun <1212malka@gmail.com>
2022-01-11 12:30:38 -0500

    added bootstrap cards

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,24 +1,36 @@
 import React from "react";
-
+// import { Navbar, Nav, Container, Card } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
 
 const About = () => {
-    return (
-      <div>
+  return (
+    <div>
+      <>
+        <Card>
         <h1>About:</h1>
-        <p>This app was inspired by my boyfriend and his passion for films. On every
-          Sunday, he meets up with his friends online and they all watch a movie. Every 
-          person has a chance to pick a movie for the group and a common problem they face
-          is keeping track of the movies they've seen. Flix-Fix is an app to answer to this 
-          problem! 
-          
-         
-
-          
-        </p>
-        </div>
+          <Card.Img variant="top" width="100%" height="auto" src="https://media.giphy.com/media/mcJIq9k8JOcJG/giphy.gif" />
+          <Card.Body>
+            <Card.Text>
+          This app was inspired by my boyfriend and his passion for films. Every
+          Sunday evening, he meets up with his friends online and they all watch a movie. Each week,
+          a different person has a chance to pick a movie for the group. Though, a common problem they 
+          face is keeping track of the movies they've seen. Flix-Fix is an app to answer to this
+          problem!
 
 
 
-    );
-  };
+
+      
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        
+      </>
+
+    </div>
+
+
+
+  );
+};
 export default About;
