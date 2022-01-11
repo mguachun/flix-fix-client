e765332b0d668cf96208b6b4759f919f9927079c
Melissa Guachun <1212malka@gmail.com>
2022-01-10 21:39:56 -0500

    style inlcuded

--- src/App.js
+++ src/App.js
@@ -6,8 +6,38 @@ import About from "./containers/About";
 import Home from "./containers/Home";
 import HowTo from "./components/HowTo"
 import MovieForm from "./components/forms/MovieForm"
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+/>
+// import './custom.scss';
 
+
+
+// function App() {
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://[::1]:3000")
+    .then(resp => resp.json())
+    .then(m => {
+      this.setState({
+        movies: m
+      })
+    })
+    
+  }
+
+render() {
   return (
     <div>
       <NavBar />
@@ -18,8 +48,11 @@ function App() {
         <Route path="/how-to" element={<HowTo />} />
         
     </Routes>
+
+ 
     </div>
   );
+}
 }
 
 export default App;
