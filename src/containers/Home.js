62ea78f2a8fc862068df1a53f55dc55620cea6a5
Melissa Guachun <1212malka@gmail.com>
2022-01-12 18:59:12 -0500

    moved componentdidmount

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,28 +1,67 @@
 import React from "react";
 // import StarRating from "../StarRating";
 
-const Home = () => {
-    return (
-      <div className="movie">
-        
-        <p
-        style={{
-          
-          textAlign: "center",
-          fontSize: 21,
-       
-  
-        }}
-      >
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      movies: []
+      //this.props.title
 
-        <h3 style={{textAlign: "center"}}></h3>is an application that helps you keep track of 
-        your movie queue. It allows you to view movies you've seen in the past and logs shows you have
-        planned for the future. 
+      
+    };
+  }
 
-      </p> 
-      {/* <StarRating /> */}
+  componentDidMount() {
+    fetch("http://localhost:3000/movies")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            movies: result.movies
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+  render() {
+    const { error, isLoaded, movies } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      
+      return (
+        <li>
+          {this.props.movies.map(movie => (
+            <li key={movie.id}>
+              {movie.title} {movie.year}
+            </li>
+          ))}
+        </li>
+        // return this.props.banks.map((bank) => <div className="bank-list-data">
+        // <ol>
+        //  {bank.name} is a {bank.gender} 🐖 with ${bank.fund} 💰
+      );
+    }
+  }
 
-      </div>
-    );
-  };
+}
+
+
+
+
+
 export default Home;
