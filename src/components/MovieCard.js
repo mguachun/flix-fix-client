831fb4696518e9f28264d482ee4ddb567c677628
Melissa Guachun <1212malka@gmail.com>
2022-01-20 00:46:15 -0500

    movie card made for each movie obj

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const MovieCard = ({title, year}) => {
+const MovieCard = ({title, year, image}) => {
     return (
         <div>
             <h2>Title:{title}</h2>
             <h2>Year:{year}</h2>
+            <h2>Image:{image}</h2>
         </div>
     )
 }
