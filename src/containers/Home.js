6439fbeab3b03920af9621cf2f7d54d09d2045ca
Melissa Guachun <1212malka@gmail.com>
2022-01-20 22:39:34 -0500

    handle function for post in home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
+import MovieForm from "../components/forms/MovieForm";
 import MovieCard from "../components/MovieCard";
+
 export default function Home () {
   
   const [movies, setMovies] = useState([]);
-  // const fetchMovies = async () => {
-  //   const response = await axios("http://localhost:3000/movies");
-  //   setMovies(response.data);
-  // }
+
   
+  function handleAddMovie(addedMovie) {
+    setMovies((movies) => [...movies, addedMovie]);
+  }
   
   useEffect(() => {
     fetch("http://localhost:3000/movies")
@@ -20,6 +22,7 @@ export default function Home () {
   return movies ?(
 
     <div>
+      <MovieForm onAddMovie={handleAddMovie} />
       <h1>Movies:</h1>
     {  movies.map( (movie) => <MovieCard title={movie.title} year={movie.year} director={movie.director} review={movie.review} genre={movie.genre}/> ) }
 
