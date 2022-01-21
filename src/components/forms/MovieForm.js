7a8180fc38ac014ac29078ebfdfc535288374655
Melissa Guachun <1212malka@gmail.com>
2022-01-20 22:39:18 -0500

    set initial state

--- src/components/forms/MovieForm.js
+++ src/components/forms/MovieForm.js
@@ -1,91 +1,94 @@
-import { Component } from 'react';
-export default function MovieForm () {
-    const [title, setTitle] = useState("")
-    const [director, setDirector] = useState("")
-    const [year, setYear] = useState("")
-    const [stars, setStars] = useState("")
-    const [review, setReview] = useState("")
-    const [genre, setGenre] = useState("")
+import { useState } from "react";
 
-  
+const initialState = {
+    title: "",
+    director: "",
+    year: "",
+    stars: "",
+    review: "",
+    genre: "",
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const movieData = {title, director, year, stars, review, genre}
-    
-        fetch("http://localhost:3000/movies", {
-            method: 'POST',
-            body: JSON.stringify(movieData),
-            headers: {
-                Content_Type: "application/json",
-            }
-        })
-    // state = {
-    //     title: '',
-    //     director: '',
-    //     year: '',
-    //     stars: '',
-    //     review: '',
-    //     genre: ''
-    // };
+  function MovieForm({ onAddMovie }) {
+    const [formData, setFormData] = useState(initialState);
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
         });
-    };
-
-    // handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log(this.state.title, this.state.director, this.state.year, this.state.stars,
-    //         this.state.review, this.state.genre);
-    //     this.setState({
-    //         title: '',
-    //         director: '',
-    //         year: '',
-    //         stars: '',
-    //         review: '',
-    //         genre: ''
-    //     });
-    // };
-
-
-
-
-        return (
-            <div className="form-container">
-                <form onSubmit={this.handleSubmit}>
-                <h1>Track a Movie</h1>
-                <div>
-                <label>Title:</label>
-                <input type="text" value={this.state.title} onChange={this.handleChange} name="title"/>
-                </div>
-                <br></br>
-                <div>
-                <label>Director:</label>
-                <input type="text" value={this.state.director} onChange={this.handleChange} name="director"/>
-                </div>
-                <br></br>
-                <div>
-                <label>Year:</label>
-                <input type="text" value={this.state.year} onChange={this.handleChange} name="year"/>
-                </div>
-                <br></br>
-                <div>
-                <label>Review:</label>
-    
-                <input type="text" value={this.state.review} onChange={this.handleChange} name="review"/>
-                </div>
-                <br></br>
-                <div>
-                <label>Genre:</label>
-                <input type="text" value={this.state.genre} onChange={this.handleChange} name="genre"/>
-                </div>
-                <br></br>
-                <input type="submit" value="Submit" />
-
-                </form>
-            </div>
-        )
     }
-}
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("http://localhost:3000/movies", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        })
+        .then((r) => r.json())
+        .then((newMovie) => {
+            setFormData(initialState);
+            onAddMovie(newMovie);
+
+        });
+    }
+    return (
+        <div className="card">
+            <h2>Track a Flick</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title</label>
+                <input
+                type="text"
+                id="title"
+                value={formData.title}
+                onChange={handleChange}
+                />
+                  <label htmlFor="director">Director</label>
+                <input
+                type="text"
+                id="director"
+                value={formData.director}
+                onChange={handleChange}
+                />
+                  <label htmlFor="year">Year</label>
+                <input
+                type="text"
+                id="year"
+                value={formData.year}
+                onChange={handleChange}
+                />
+                  <label htmlFor="stars">Stars</label>
+                <input
+                type="text"
+                id="stars"
+                value={formData.stars}
+                onChange={handleChange}
+                />
+                  <label htmlFor="review">Reveiw</label>
+                <input
+                type="text"
+                id="review"
+                value={formData.review}
+                onChange={handleChange}
+                />
+                  <label htmlFor="genre">Genre</label>
+                <input
+                type="text"
+                id="genre"
+                value={formData.genre}
+                onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+
+            </form>
+        </div>
+    );
+
+  }
+
+
+
+export default MovieForm;
